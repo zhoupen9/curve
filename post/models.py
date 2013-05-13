0b5f6dbd7a56d7197c699a449f280fd9b4c8ab5a
Peng Zhou <zhou.pen9@gmail.com>
2013-05-14 02:03:22 +0800

    Static files moved to a new created folder BASE_DIR/shared-static, create media folder and static folder.
    Site sprites png file modified to add cross icons and other icons.
    Create post, tag models in sub app "post" directory.

--- post/models.py
+++ post/models.py
@@ -1,3 +1,26 @@
+
+from django.contrib.auth import User
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    """
+    Post tags.
+    Posts and tags' relationship is a many-to-many relationship,
+    Since django models creates backward reference, we only create
+    that many-to-many relationship on the other end.
+    """
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=32, unique=True)
+    
+class Post(models.Model):
+    """
+    Post.
+    """
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256)
+    tag = models.ManyToManyField(Tag)
+    createTime = models.DateTimeField()
+    author = models.ForeignKey(User)
+    content = models.TextField()
