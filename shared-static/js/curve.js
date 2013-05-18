218386ca034d0edd7c2fd115a430549ea39db07f
Zhou Peng <zhou.pen9@gmail.com>
2013-05-18 21:46:04 +0800

    add some ui java scripts.

--- shared-static/js/curve.js
+++ shared-static/js/curve.js
@@ -3,36 +3,8 @@
 $(document).ready(function () {
     "use strict";
     // setup all draggable elements' event handlers.
-    $('.draggable .dialog-header').each(function (index, element) {
-        $(this).mousedown(function (e) {
-            var startOffset = $(this).parent().parent().offset();
-            $(this).data('dragging', true);
-            $(this).data('offsetX', e.pageX - startOffset.left);
-            $(this).data('offsetY', e.pageY - startOffset.top);
-            $('#debug-output-panel').text($(this).parent().parent().attr('class'));
-            $(document).bind('mousemove.' + 
-            return false;
-        });
-        
-        $(this).mousemove(function (e) {
-            if ($(this).data('dragging')) {
-                //var oX = e.clientX - iX, oY = e.clientY - iY;
-                var x = e.pageX - $(this).data('offsetX'), y = e.pageY - $(this).data('offsetY');
-                $('#debug-output-panel').text('pageX:' + e.pageX + ', pageY:' + e.pageY + ', x:' + x + ', y:' + y);
-                $(this).parent().parent().css({"left": x + "px", "top": y + "px"});
-                return false;
-            }
-            return true;
-        });
-        
-        //$(this
-        
-        $(this).mouseup(function (e) {
-            $(this).removeData('dragging');
-            //window.releaseCapture();
-            //window.releaseEvents(Event.MOUSEMOVE|Event.MOUSEUP);
-            e.cancelBubble = true;
-        });
+    $('.draggable').each(function (index, element) {
+        $(this).draggable();
     });
     
     $('#global-post-btn').click(function () {
--- shared-static/js/draggable.js
+++ shared-static/js/draggable.js
@@ -3,48 +3,55 @@
 
 (function ($) {
     "use strict";
+    
+    //$.support.selectstart = document.createElement("div").hasOwnProperty('onselectstart');
     // jquery draggable.
     $.fn.draggable = function () {
-        function isIntersect(e) {
-            
-        }
-        var that = this;
+        var that = this, header = $(this).find('.dialog-header');
+        
+        // prevent selection.
+//        $(this).on($.support.selectstart ? 'selectstart' : 'mousedown', function (e) {
+//            e.preventDefault();
+//        });
+        
+        // dialog close button click handler.
+        $(this).find('.dialog-close').click(function (e) {
+            $(this).closest('.dialog').hide();
+            $(this).closest('.dialog-container').hide();
+        });
+        
         $(this).mousedown(function (e) {
-            // test if mouse is over header.
-            var that = $(this).find('.dialog-close'), c = that.get();
-            if (that && (c == e.target || $.contains(c, e.target))) {
-                $(this).hide();
+            // test if mouse is over header.            
+            if (header[0] === e.target || $.contains(header[0], e.target)) {
+                $(this).data('dragging', true);
+                $(this).data('offsetX', e.pageX - $(this).offset().left);
+                $(this).data('offsetY', e.pageY - $(this).offset().top);
+            
+                // handle parent mouse move to move draggable.
+                $(':parent').on('mousemove', function (e) {
+                    if ($(that).data('dragging')) {
+                        var x = e.pageX - $(that).data('offsetX'),
+                            y = e.pageY - $(that).data('offsetY');
+                        $(that).css({"position": "relative", "left": x + "px", "top": y + "px"});
+                    }
+                });
+                
+                // handle parent mouse up to release dragging.
+                $(':parent').on('mouseup', function (e) {
+                    if ($(that).data('dragging')) {
+                        $(that).removeData('dragging');
+                    }
+                });
             }
-//            
-//            var startOffset = $(this).offset();
-//            $(this).data('dragging', true);
-//            $(this).data('offsetX', e.pageX - startOffset.left);
-//            $(this).data('offsetY', e.pageY - startOffset.top);
-//            $('#debug-output-panel').text($(this).attr('class'));
-//        
-//            $(':parent').mousemove(function (e) {
-//                if ($(that).data('dragging')) {
-//                    var x = e.pageX - $(that).data('offsetX'), y = e.pageY - $(that).data('offsetY');
-//                    $(that).css({"left": x + "px", "top": y + "px"});
-//                }
-//            });
-//            $(':parent').mouseup(function (e) {
-//                if ($(that).data('dragging')) {
-//                    $(that).removeData('dragging');
-//                }
-//            });
-//            return false;
         });
         
         $(this).mousemove(function (e) {
             if ($(this).data('dragging')) {
                 //var oX = e.clientX - iX, oY = e.clientY - iY;
-                var x = e.pageX - $(this).data('offsetX'), y = e.pageY - $(this).data('offsetY');
-                $('#debug-output-panel').text('pageX:' + e.pageX + ', pageY:' + e.pageY + ', x:' + x + ', y:' + y);
+                var x = e.pageX - $(this).data('offsetX'),
+                    y = e.pageY - $(this).data('offsetY');
                 $(this).css({"left": x + "px", "top": y + "px"});
-                return false;
             }
-            return true;
         });
         
         $(this).mouseup(function (e) {
@@ -52,7 +59,7 @@
             //window.releaseCapture();
             //window.releaseEvents(Event.MOUSEMOVE|Event.MOUSEUP);
             $(':parent').off('mousemove mouseup');
-            //$(this).off('mousemove mouseup');
+            $(this).off('mousemove mouseup');
             e.cancelBubble = true;
         });
         
