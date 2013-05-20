158df50e3ea2b0f569b3e0127f3dc5fcc47833d5
Peng Zhou <zhou.pen9@gmail.com>
2013-05-21 03:11:19 +0800

    Modify plugin base.

--- shared-static/js/curve.js
+++ shared-static/js/curve.js
@@ -38,35 +38,83 @@
             return this;
         }
     });
-    
-    $.plugin = $.plugin || {};
-    
-    $.Plugin = function () {};
-    
+
+    // Add plugin to Jquery.
+    $.Plugin = $.Plugin || {};
+
+    // Define Plugin prototype.
     $.Plugin.prototype = {
         name: 'plugin',
         version: '0.1',
+
+	// Plugin override this method to create.
         create: $.noop,
+
+	// Plugin override this method to initialize.
         init: $.noop,
-        destory: $.noop
+
+	// Plugin override this method to destroy anything if need to destory.
+        destory: $.noop,
+
+	// create plugin.
+	createPlugin: function (options, elem) {
+	    this.create(options, elem);
+	    this.init(options, elem);
+	},
     };
-    
-    $.Plugin.plugin = function (name, proto) {
-        var Constructor = $.fn[name] = function (options, elem) {
-            if (!this.create) {
-                return new Constructor(options, elem);
-            }
-            if (arguments.length) {
-                return this.create(arguments);
-            }
+
+    // create new plugin.
+    $.plugin = function (name, proto) {
+	var constructor, proxyPrototype = {};
+	if (!name) {
+	    return $.error("Plugin must declare a name.");
+	}
+	
+	// // check if plugin with name already exists.
+	// if (this.plugins[name]) {
+	//     return this.plugins[name];
+	// }
+
+	// create new constructor for plugin.
+        constructor = function (options, elem) {
+	    if (!this.createPlugin) {
+                return new constructor(options, elem);
+	    }
+	    if (arguments.length) {
+                this.createPlugin(options, elem);
+	    }
         };
-        
+
+	// copy prototype to proxy prototype.
         $.each(proto, function (prop, value) {
-            if (!$.isFunction(value)) {
-                this[prop] = value;
-            } else {
-                
-            }
+	    if (!$.isFunction(value)) {
+                proxyPrototype[prop] = value;
+	    } else {
+                proxyPrototype[prop] = (function () {
+		    var retValue;
+		    retValue = value.apply(this, arguments);
+		    return retValue;
+		});
+	    }
         });
-    };
+
+	// create construtor's prototype using proxy prototype.
+	constructor.prototype = $.extend($.Plugin.prototype, proxyPrototype);
+
+	// create bridge to Jquery.
+	$.fn[name] = function (options) {
+	    var retValue = this;
+
+	    this.each(function () {
+		var instance = $.data(this, name);
+		if (instance) {
+		    instance.init();
+		} else {
+		    $.data(this, name, new constructor(options, this));
+		}
+	    });
+	    return retValue;
+	}
+    };    
+    
 }($));
--- shared-static/js/draggable.js
+++ shared-static/js/draggable.js
@@ -4,21 +4,26 @@
 (function ($) {
     "use strict";
     
-    $.Plugin.plugin('draggable', {
+    $.plugin('draggable', {
         /* Default options. */
         options: {
         },
         
         /* create draggable. */
-        create: function () {
-            return this;
+        create: function (options, elem) {
+	    if (options) {
+		$.extend(this, options);
+	    }
+	    if (elem) {
+		return $.extend($(elem), this);
+	    }
         },
         
         /* Initialize draggable. */
-        init: function () {
-            $(this).delegate('mousedown', this.handleMouseDown);
-            $(this).delegate('mousemove', this.handleMouseMove);
-            $(this).delegate('mouseup', this.handleMouseUp);
+        init: function (options, elem) {
+            $(elem).on('mousedown', '.dialog-header', this.handleMouseDown);
+            $(elem).on('mousemove', '.dialog-header', this.handleMouseMove);
+            $(elem).on('mouseup', '.dialog-header', this.handleMouseUp);
         },
         
         /* Destroy draggable. */
@@ -27,44 +32,53 @@
         },
         
         handleMouseMove: function (e) {
-            if ($(this).data('dragging')) {
+	    var x, y, parent = $(this).closest('.draggable');
+	    if ($(this).data('dragging')) {
                 //var oX = e.clientX - iX, oY = e.clientY - iY;
-                var x = e.pageX - $(this).data('offsetX'),
-                    y = e.pageY - $(this).data('offsetY');
-                $(this).css({"left": x + "px", "top": y + "px"});
-            }
+                x = e.pageX - $(this).data('offsetX');
+                y = e.pageY - $(this).data('offsetY');
+                parent.css({"left": x + "px", "top": y + "px"});
+	    }
         },
         
         handleMouseDown: function (e) {
-            var that = this, header = $(this).find('.dialog-header');
-            // test if mouse is over header.            
-            if (header[0] === e.target || $.contains(header[0], e.target)) {
-                $(this).data('dragging', true);
-                $(this).data('offsetX', e.pageX - $(this).offset().left);
-                $(this).data('offsetY', e.pageY - $(this).offset().top);
+            $(this).data('dragging', true);
+            $(this).data('offsetX', e.pageX - $(this).offset().left);
+            $(this).data('offsetY', e.pageY - $(this).offset().top);
+            // var that = this, header = $(this).find('.dialog-header');
+            // // test if mouse is over header.            
+            // if (header[0] === e.target || $.contains(header[0], e.target)) {
+            //     $(this).data('dragging', true);
+            //     $(this).data('offsetX', e.pageX - $(this).offset().left);
+            //     $(this).data('offsetY', e.pageY - $(this).offset().top);
             
-                // handle parent mouse move to move draggable.
-                $(':parent').on('mousemove', function (e) {
-                    if ($(that).data('dragging')) {
-                        var x = e.pageX - $(that).data('offsetX'),
-                            y = e.pageY - $(that).data('offsetY');
-                        $(that).css({"position": "relative", "left": x + "px", "top": y + "px"});
-                    }
-                });
+            //     // handle parent mouse move to move draggable.
+            //     $(':parent').on('mousemove', function (e) {
+            //         if ($(that).data('dragging')) {
+            //             var x = e.pageX - $(that).data('offsetX'),
+            //                 y = e.pageY - $(that).data('offsetY');
+            //             $(that).css({"position": "relative", "left": x + "px", "top": y + "px"});
+            //         }
+            //     });
                 
-                // handle parent mouse up to release dragging.
-                $(':parent').on('mouseup', function (e) {
-                    if ($(that).data('dragging')) {
-                        $(that).removeData('dragging');
-                    }
-                });
-            }
+            //     // handle parent mouse up to release dragging.
+            //     $(':parent').on('mouseup', function (e) {
+            //         if ($(that).data('dragging')) {
+            //             $(that).removeData('dragging');
+            //         }
+            //     });
+            // }
         },
         
         handleMouseUp: function (e) {
-            $(this).removeData('dragging');
-            $(':parent').off('mousemove mouseup');
-            $(this).off('mousemove mouseup');
+	    var draggable;
+	    if ($(this).data('draggable')) {
+		$(this).off('mousemove mouseup');
+	    } else {
+		draggable = $(this).find('.draggable');
+	    }
+            draggable.removeData('dragging');
+            draggable.off('mousemove mouseup');
             e.cancelBubble = true;
         }
     });
